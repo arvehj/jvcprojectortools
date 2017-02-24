2797b787225d4bb07046ba538784d39c11aa1fc7
Arve Hjønnevåg <src-jvcprojectortools.arve@fm.hjonnevag.com>
2017-02-23 21:10:38 -0800

    eotf_bt1886: Add set_black function rename alternate function

--- eotf_bt1886.py
+++ eotf_bt1886.py
@@ -8,19 +8,22 @@ peak = 100
 
 gamma = 2.4
 
-Lw = 1
-#Lb = Lw/50000
-#Lb = Lw/2600000
-Lb = Lw/20000
-a = (Lw ** (1/gamma) - Lb ** (1/gamma)) ** gamma
-b = Lb ** (1/gamma) / (Lw ** (1/gamma) - Lb ** (1 /gamma))
+def set_black(l):
+    global Lb, a, b, Vc, k, a1, a2
+    Lw = 1
+    Lb = l
 
-Vc = 0.35
-a1 = 2.6
-a2 = 3.0
-k = Lw / (1 + b) ** a1
+    a = (Lw ** (1/gamma) - Lb ** (1/gamma)) ** gamma
+    b = Lb ** (1/gamma) / (Lw ** (1/gamma) - Lb ** (1 /gamma))
 
-def L1(V):
+    Vc = 0.35
+    a1 = 2.6
+    a2 = 3.0
+    k = Lw / (1 + b) ** a1
+
+set_black(1/20000)
+
+def Lt(V):
     """ITU-R BT.1886 EOTF"""
     if V < Vc:
         return k * (Vc + b) ** (a1 - a2) * (V + b) ** a2
@@ -39,7 +42,7 @@ def main():
     print('b', b)
     for i in range(11):
         i = i / 10
-        print('L * 100', i, L(i) * 100, L2(i) * 100, i ** 2.4 * 100)
+        print('L * 100', i, L(i) * 100, Lt(i) * 100, i ** 2.4 * 100)
 
 if __name__ == "__main__":
     main()
